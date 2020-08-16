3d6d2ed90c6b3cbd810adf93cfa351eacfa996eb
carlCarlson6 <bullet.carlos@gmail.com>
2020-08-16 11:56:45 +0200

    working with subscriptions

--- source/index.ts
+++ source/index.ts
@@ -1,4 +1,4 @@
-import {ApolloServer, gql, ServerInfo} from 'apollo-server'
+import {ApolloServer, gql, ServerInfo, PubSub} from 'apollo-server'
 import DbClient from './Repository/DbClient';
 import { DocumentNode } from 'graphql';
 
@@ -38,23 +38,38 @@ const main = async () => {
             register(userInfo: UserInfo!): RegisterResponse!
             login(userInfo: UserInfo!): Boolean!
         }
+
+        type Subscription {
+            newUser: User!
+        }
     `;
+
+    const NEW_USER = 'NEW_USER';
 
     const resolvers = {
         Query: {
             hello: (parent: any, {name}: any) => `hello ${name}`,
             user: () => ({id: 1, username: 'bob'})
         },
+        
         Mutation: {
-            register: (parent: any, {userInfo: {username}}: any, context: any, info: any) => ({
-                error: null,
-                user: {username}
-            }),
+            register: (parent: any, {userInfo: {username}}: any, context: any, info: any) => {
+                const user = {username}
+                context.pubsub.publish(NEW_USER, {newUser:user })
+                return { error: null, user }
+            },
             login: (parent: any, args: any, context: any, info: any) => {
                 console.log(parent);
                 return true;
             }
         },
+
+        Subscription: {
+            newUser: {
+                subscribe: (parent: any, args: any, {pubsub}: any, info: any) => pubsub.asyncIterator()(NEW_USER)
+            }
+        },
+
         User: {
             id: () => {
                 return (Math.floor(Math.random() * (99999999999999999999999 - 1 + 1)) + 1);
@@ -66,12 +81,12 @@ const main = async () => {
         }
     }
 
-
+    const pubsub = new PubSub();
     
     const server = new ApolloServer({
         typeDefs, 
         resolvers,
-        context: ({req, res}) => ({req, res})
+        context: ({req, res}) => ({req, res, pubsub})
     })
     const serverInfo: ServerInfo = await server.listen(4000)
     console.log('server is running on', serverInfo.url)
